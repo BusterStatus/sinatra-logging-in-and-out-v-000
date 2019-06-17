fda44c8106efe82b652656f637c89bf9bf40cd6c
Matthew Serre <matthew.serre@gmail.com>
2019-06-17 20:45:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,10 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(email: params["email"], password: params["password"])
     session[:user_id] = @user.id
-    redirect '/account'
+    if session[:user_id] == @user.id
+      redirect '/account'
+    end
+    erb :error
   end
 
   get '/account' do
